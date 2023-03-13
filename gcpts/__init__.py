b6aa1c40004ae5399cf1b4e35e0e3041e70acaae
richwomanbtc <richwomanbtc@gmail.com>
2023-03-13 17:43:40 +0400

    add __version__

--- gcpts/__init__.py
+++ gcpts/__init__.py
@@ -3,6 +3,8 @@ from gcpts.sql.basic import Query
 from gcpts.sql.resample import ResampleQuery
 from google.cloud import bigquery
 
+__version__ = "0.1.0"
+
 
 class GCPTS(Query, ResampleQuery, Uploader):
     def __init__(self, project_id, dataset_id, *args, **kwargs):
